3d4b99a34da8ff4d61b3e13a487bd69866433f01
coreentin <core.entin@gmail.com>
2016-02-18 20:13:29 +0100

    first work cm-12.1

---
+++ BoardConfig.mk
@@ -0,0 +1,84 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+# inherit from the proprietary version
+-include vendor/nvidia/s8515/BoardConfigVendor.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/nvidia/s8515/include
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := $(TARGET_CPU_VARIANT)
+
+# Board
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := t114
+TARGET_TEGRA_FAMILY := t11x
+TARGET_NO_BOOTLOADER := true
+#TARGET_NO_RADIOIMAGE := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+BOARD_DISABLE_TRIPLE_BUFFERED_DISPLAY_SURFACES := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/nvidia/s8515
+TARGET_KERNEL_CONFIG := tegra_s8515_cpasjuste_defconfig
+BOARD_KERNEL_CMDLINE := "androidboot.selinux=permissive"
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_USERDATAIMAGE_PARTITION_SIZE  := 2145386496
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 805306368
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+BOARD_HARDWARE_CLASS := device/nvidia/s8515/cmhw/
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := s8515
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nvidia/s8515/bluetooth
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_STA          := "/data/misc/wifi/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/data/misc/wifi/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0"
+WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
