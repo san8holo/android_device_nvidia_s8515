495b791db1ab1399d611313eb92ca15e2207738f
san8holo <san8holo@gmail.com>
2016-02-29 13:20:49 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,19 @@
 # inherit from the proprietary version
 -include vendor/nvidia/s8515/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := false
+BOARD_HAVE_BLUETOOTH_BCM := false
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nvidia/s8515/bluetooth
+
+# Board
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := t114
+TARGET_TEGRA_FAMILY := t11x
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := s8515
+
 TARGET_SPECIFIC_HEADER_PATH := device/nvidia/s8515/include
 
 # Architecture
@@ -30,13 +43,6 @@ TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := $(TARGET_CPU_VARIANT)
-
-# Board
-TARGET_BOARD_PLATFORM := tegra
-TARGET_TEGRA_VERSION := t114
-TARGET_TEGRA_FAMILY := t11x
-TARGET_NO_BOOTLOADER := true
-#TARGET_NO_RADIOIMAGE := true
 
 # Graphics
 USE_OPENGL_RENDERER := true
@@ -49,7 +55,6 @@ EXTENDED_FONT_FOOTPRINT := true
 TARGET_KERNEL_SOURCE := kernel/nvidia/s8515
 TARGET_KERNEL_CONFIG := tegra_s8515_cpasjuste_defconfig
 BOARD_KERNEL_CMDLINE := "androidboot.selinux=permissive"
-
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_USERDATAIMAGE_PARTITION_SIZE  := 2145386496
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 805306368
@@ -59,16 +64,10 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_HAS_LARGE_FILESYSTEM := true
-
 BOARD_HARDWARE_CLASS := device/nvidia/s8515/cmhw/
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := s8515
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nvidia/s8515/bluetooth
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
@@ -82,3 +81,19 @@ WIFI_DRIVER_FW_PATH_AP           := "/data/misc/wifi/firmware/fw_bcmdhd_apsta.bi
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0"
 WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+
+#TWRP
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/nvidia/s8515/recovery/graphics.c
+TW_THEME := portrait_hdpi
+DEVICE_RESOLUTION := 720x1280
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_USE_SKIA_LCDTEXT := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_BRIGHTNESS_PATH := /sys/devices/platform/pwm-backlight/backlight/pwm-backlight/brightness
+TW_MAX_BRIGHTESS := 255
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/tegra-udc.0/gadget/lun0/file
